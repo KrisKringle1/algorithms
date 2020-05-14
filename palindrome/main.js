607151d2327cac088ff75ae16ad2da38dddbb74c
Kris <kmringler@gmail.com>
2020-05-14 21:51:56 +0000

    challenge complete

--- palindrome/main.js
+++ palindrome/main.js
@@ -10,6 +10,8 @@ var isPalindrome = function (x) {
   //parseInt string
   //if negative number return false
   //if input ends in 0 return false
+
+
   if (Math.sign(x) === -1 || -0) {
     return false;
   }
@@ -18,17 +20,24 @@ var isPalindrome = function (x) {
   let reverseString = ""
   let stringLength = numToString.length;
   let stringLengthMinusOne = numToString.length - 1;
+  if (numToString[stringLength] === '0' && stringLength > 1) {
+    return false
+  }
+
   for (let i = 0; i < numToString.length; i++) {
-    if (numToString[stringLength] === 0) {
-      return false
-    }
-    reverseString += numToString.splice(stringLengthMinusOne, stringLength);
+
+
+    reverseString += numToString.slice(stringLengthMinusOne, stringLength);
     stringLength--;
     stringLengthMinusOne--;
 
   }
+  if (parseInt(reverseString) === x) {
+    return true;
+  } else {
+    return false;
+  }
 
-  return parseInt(reverseString)
 
 
 };

1d12425b2be66f0c6fea9d741f980d98ea24411b
Kris <kmringler@gmail.com>
2020-05-16 23:09:51 +0000

    need to look at this again

--- count-primes/main.js
+++ count-primes/main.js
@@ -2,8 +2,8 @@
  * @param {number} n
  * @return {number}
  */
-var countPrimes = function (n) {
-  //break up number to count
+var countPrimes = function (initialNumber) {
+  //break up initialNumberumber to count
   //loop through array
   //check if number is divisible by 1-9
   //if yes check to see if number is n
@@ -12,11 +12,21 @@ var countPrimes = function (n) {
 
   let notPrime = 0;
   let counter = 0;
-  for (let i = 2; i < n; i++) {
-    if (n % i == 0) {
-      notPrime++;
+  debugger;
+  for (let oneThroughInitNum = 1; oneThroughInitNum < initialNumber; oneThroughInitNum++) {
+    //loop that will check each number 1 through entered number called oneThroughInitNum
+
+    for (let divider = 1; divider < oneThroughInitNum; divider++) {
+      //loop that will check if the number oneThroughInit number is divisible by 1 through that number
+      if (oneThroughInitNum % divider > 0 && oneThroughInitNum !== 1 && divider !== oneThroughInitNum) {
+        //if the number reamaining is 0 and oneThroughInit Number is not 1 and divider is not oneThroughInitItem the number is not prime
+        notPrime++
+      } else {
+        counter++
+      }
+
     }
-    counter++;
+
   }
   return counter;
 }

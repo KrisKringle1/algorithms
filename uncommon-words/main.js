9e6e72b4fa35048aa02f473996f8b29b7c97a4e3
Kris <kmringler@gmail.com>
2020-05-25 03:20:51 +0000

    initial commit added

---
+++ uncommon-words/main.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} A
+ * @param {string} B
+ * @return {string[]}
+ */
+var uncommonFromSentences = function (firstString, secondString) {
+  //create a for loop with two iterators
+  //one that iterates through first string, another that iterates through the 2nd string
+  //push each letter into a new string until you hit a space
+  //compare the two strings, if they are the same, continue
+  //if they are different, return the two words
+  //if you reach the end of the loop
+
+};

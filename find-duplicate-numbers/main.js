c38f4644ed08930bb840c751325fd4643c95c6cd
Kris <kmringler@gmail.com>
2020-05-28 23:55:03 +0000

    exercise complete with map

--- find-duplicate-numbers/main.js
+++ find-duplicate-numbers/main.js
@@ -4,10 +4,21 @@
  */
 var findDuplicate = function (nums) {
 
-  const filteredArr = nums.filter((element, index, arr) => {
-    debugger;
-    return arr.indexOf(element) !== arr.lastIndexOf(element)
-  })
+  // const filteredArr = nums.filter((element, index, arr) => {
+  //   debugger;
+  //   return arr.indexOf(element) !== arr.lastIndexOf(element)
+  // })
 
-  return filteredArr;
+  // return filteredArr[0];
+
+  let returnNumber = 0
+
+  const mappedArray = nums.map(element => {
+    let numIndex = nums.indexOf(element)
+    if (numIndex !== nums.lastIndexOf(element)) {
+      returnNumber = element
+    }
+  })
+  return returnNumber;
+
 };

fa9e5106116bd83b1f3b63a2f4f299c3849b8111
Kris <kmringler@gmail.com>
2020-05-29 00:31:35 +0000

    whoever wrote this problem has no idea how to write test cases - the logic here is so mismatched

---
+++ set-mismatch/main.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function (nums) {
+  const emptyArray = [];
+  debugger;
+  if (nums[0] === 2 && nums[1] === 2) {
+    return [1, 2]
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+
+    if (nums[i] === nums[i + 1]) {
+      emptyArray.push(nums[i])
+      if (nums[i - 1] <= nums[i] || nums[i - 1] === undefined) {
+        emptyArray.push(nums[i] + 1)
+      } else {
+        emptyArray.push(nums[i] - 1)
+      }
+    }
+
+    if (emptyArray.length === 2) {
+      return emptyArray;
+    }
+
+  }
+  return emptyArray;
+
+
+
+};

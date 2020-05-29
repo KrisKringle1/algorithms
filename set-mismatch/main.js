8671f69786d00642a2ae0b8efe8d3bb858191649
Kris <kmringler@gmail.com>
2020-05-29 01:08:18 +0000

    logic is almost there

--- set-mismatch/main.js
+++ set-mismatch/main.js
@@ -3,29 +3,49 @@
  * @return {number[]}
  */
 var findErrorNums = function (nums) {
-  const emptyArray = [];
-  debugger;
-  if (nums[0] === 2 && nums[1] === 2) {
-    return [1, 2]
-  }
+  // const emptyArray = [];
+  // debugger;
+  // if (nums[0] === 2 && nums[1] === 2) {
+  //   return [1, 2]
+  // }
+
+  // for (let i = 0; i < nums.length; i++) {
+
+  //   if (nums[i] === nums[i + 1]) {
+  //     emptyArray.push(nums[i])
+  //     if (nums[i - 1] <= nums[i] || nums[i - 1] === undefined) {
+  //       emptyArray.push(nums[i] + 1)
+  //     } else {
+  //       emptyArray.push(nums[i] - 1)
+  //     }
+  //   }
+
+  //   if (emptyArray.length === 2) {
+  //     return emptyArray;
+  //   }
+
+  // }
+  // return emptyArray;
+  const emptyArray = []
+
 
   for (let i = 0; i < nums.length; i++) {
-
-    if (nums[i] === nums[i + 1]) {
+    if (nums.indexOf(nums[i]) !== nums.lastIndexOf(nums[i])) {
       emptyArray.push(nums[i])
-      if (nums[i - 1] <= nums[i] || nums[i - 1] === undefined) {
-        emptyArray.push(nums[i] + 1)
-      } else {
+      if (!nums.includes(nums[i] - 1)) {
         emptyArray.push(nums[i] - 1)
-      }
-    }
 
-    if (emptyArray.length === 2) {
-      return emptyArray;
+      } else if (!nums.includes(nums[i] + 1)) {
+        emptyArray.push(nums[i] + 1)
+      }
+
     }
 
   }
+
   return emptyArray;
+
+
 
 
 

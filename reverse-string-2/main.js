971e3d43ee3789e1e7edb8b358aeeb529cc7e579
Kris <kmringler@gmail.com>
2020-06-02 19:32:46 +0000

    redoing logic but the exercise is there

--- reverse-string-2/main.js
+++ reverse-string-2/main.js
@@ -3,47 +3,87 @@
  * @param {number} k
  * @return {string}
  */
-var reverseStr = function (s, k) {
-  let newIndexStart = k
-  let reverseIndex = k - 1
-  let kCopy = k
-  let index = 0;
-  let emptyString = ""
-  let whenToReverse = 0
-  if (s.length === 1 || k === 1) {
-    return s;
-  }
-  if (k >= s.length) {
-    reverseIndex = s.length - 1
-    while (reverseIndex !== -1) {
-      emptyString += s[reverseIndex];
-      reverseIndex--;
-    }
-    return emptyString;
-  }
-  while (index < s.length) {
+// var reverseStr = function (s, k) {
+//   let newIndexStart = k
+//   let reverseIndex = k - 1
+//   let kCopy = k
+//   let index = 0;
+//   let emptyString = ""
+//   let whenToReverse = 0
+//   if (s.length === 1 || k === 1) {
+//     return s;
+//   }
+//   if (k >= s.length) {
+//     reverseIndex = s.length - 1
+//     while (reverseIndex !== -1) {
+//       emptyString += s[reverseIndex];
+//       reverseIndex--;
+//     }
+//     return emptyString;
+//   }
+//   while (index < s.length) {
 
-    if (kCopy) {
-      if (s[reverseIndex] === undefined) {
-        return emptyString += s[s.length - 1]
-      }
-      emptyString += s[reverseIndex]
-      kCopy--;
-      reverseIndex--;
-      index++
+//     if (kCopy) {
+//       if (s[reverseIndex] === undefined) {
+//         return emptyString += s[s.length - 1]
+//       }
+//       emptyString += s[reverseIndex]
+//       kCopy--;
+//       reverseIndex--;
+//       index++
+//     } else {
+//       emptyString += s[newIndexStart]
+//       newIndexStart++
+//       index++
+//       whenToReverse++;
+//     }
+//     if (whenToReverse === k) {
+//       whenToReverse = 0;
+//       reverseIndex = newIndexStart + 1;
+//       kCopy = k
+//       newIndexStart += k;
+//     }
+
+//   }
+//   return emptyString;
+// };
+
+
+const reverseStr = (s, k) => {
+  //create method that reverses string
+  //slice up to k into the string
+  //reverse the slice and append to empty string
+  //slice out k amount in s and append to empty string,
+  let emptyString = "";
+  let reverseIndicator = false;
+  const reverse = (string) => {
+    for (let i = string.length - 1; i > -1; i--) {
+      emptyString += string[i]
+    }
+    return emptyString
+  }
+  let i = 0;
+  let kCopy = k;
+
+  while (emptyString.length !== s.length) {
+    debugger;
+
+    if (!reverseIndicator) {
+      emptyString += s.slice(i, kCopy)
+      reverseIndicator = true;
+      i += k;
+      kCopy += k;
     } else {
-      emptyString += s[newIndexStart]
-      newIndexStart++
-      index++
-      whenToReverse++;
-    }
-    if (whenToReverse === k) {
-      whenToReverse = 0;
-      reverseIndex = newIndexStart + 1;
-      kCopy = k
-      newIndexStart += k;
+      let stringToReverse = s.slice(i, kCopy);
+      reverse(stringToReverse);
+      i += k;
+      kCopy += kCopy
+      reverseIndicator = false;
     }
 
+
   }
+
   return emptyString;
-};
+
+}
---
+++ test.js
@@ -0,0 +1,14 @@
+var obj = {
+  1: {
+    name: 'john',
+    age: 28
+  },
+  2: {
+    name: 'kris',
+    age: 24
+  },
+  3: {
+    name: 'joe',
+    age: 42
+  }
+};

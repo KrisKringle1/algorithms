a4e19ee5ff34b8f7ab6dfe42b6dbaee3b0e49087
Kris <kmringler@gmail.com>
2020-06-03 00:38:21 +0000

    explaination of this exercise doesnt make sense

--- greatest-common-divisor/main.js
+++ greatest-common-divisor/main.js
@@ -4,5 +4,18 @@
  * @return {string}
  */
 var gcdOfStrings = function (str1, str2) {
+  let checkingString = str2
+  let index = str2.length - 1
+  if (str1.includes(str2)) {
+    return str2;
+  }
+  while (str1.includes(checkingString) === false) {
+    checkingString = checkingString.slice(index, 1);
+    index--;
 
+    if (checkingString.length === 0) {
+      return ""
+    }
+  }
+  return checkingString;
 };

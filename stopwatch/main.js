9233566306d9f1dc38a3143e2ef3cbea3b0c3ebe
Kris <kmringler@gmail.com>
2020-06-05 03:44:47 +0000

    starting no longert skips seconds

--- stopwatch/main.js
+++ stopwatch/main.js
@@ -1,13 +1,13 @@
 
 
 
-const timeElement = document.getElementById('time')
 let add;
 const start = document.querySelector('.start');
 const stop = document.querySelector('.stop');
 const minutes = document.getElementById('minutes')
 const seconds = document.getElementById('seconds')
 const tenSeconds = document.getElementById('tenSeconds')
+let timeStarted = false;
 
 
 
@@ -15,27 +15,31 @@ const tenSeconds = document.getElementById('tenSeconds')
 
 
 function startTime() {
-  add = setInterval(function () {
+  if (!timeStarted) {
+    add = setInterval(function () {
 
-    if (seconds.innerText == 9) {
-      tenSeconds.innerText++
-      seconds.innerText = 0
-    } else {
-      seconds.innerText++
-    }
+      if (seconds.innerText == 9) {
+        tenSeconds.innerText++
+        seconds.innerText = 0
+      } else {
+        seconds.innerText++
+      }
 
-    if (tenSeconds.innerText == 6) {
-      minutes.innerText++
-      seconds.innerText = 0
-      tenSeconds.innerText = 0
-    }
+      if (tenSeconds.innerText == 6) {
+        minutes.innerText++
+        seconds.innerText = 0
+        tenSeconds.innerText = 0
+      }
 
 
-  }, 1000)
+    }, 1000)
+    timeStarted = true;
+  }
 }
 
 function stopTime() {
   clearInterval(add);
+  timeStarted = false;
 }
 start.addEventListener('click', startTime)
 stop.addEventListener('click', stopTime)

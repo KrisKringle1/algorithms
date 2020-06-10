4bd16d3e80a95f3166c9d06582e3596472421064
Kris <kmringler@gmail.com>
2020-06-10 21:50:17 +0000

    initial commit

---
+++ find-common-characters/main.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} A
+ * @return {string[]}
+ */
+var commonChars = function (array) {
+  //loop through array
+  //check if a letter exists in all indexes
+  //if it does, push to a new array
+  //return new array
+
+
+};

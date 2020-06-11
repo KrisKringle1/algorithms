93c308c0df1574fc533165da1ae829b18bef3aa8
Kris <kmringler@gmail.com>
2020-06-11 19:35:32 +0000

    initial commit

---
+++ lucky-int/main.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var findLucky = function (arr) {
+  //loop through array
+  //check if indexof is also last index of
+  //if it isnt, make it the lucky number
+  //if the next lucky number is larger, that is the returned number
+
+  for (let i = 0; i)
+};

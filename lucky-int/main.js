f5f502419bfcee40bbe214383b7d546e39629628
Kris <kmringler@gmail.com>
2020-06-11 19:50:22 +0000

    first problem works but wrong logic applied - redoing

--- lucky-int/main.js
+++ lucky-int/main.js
@@ -7,6 +7,21 @@ var findLucky = function (arr) {
   //check if indexof is also last index of
   //if it isnt, make it the lucky number
   //if the next lucky number is larger, that is the returned number
+  let luckyNum = -1;
 
-  for (let i = 0; i)
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i])) {
+      if (!luckyNum) {
+        luckyNum = arr[i]
+      }
+
+
+      if (luckyNum && arr[i] > luckyNum) {
+        luckyNum = arr[i];
+      }
+    }
+
+  }
+  return luckyNum
 };

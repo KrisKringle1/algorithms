809d37cc251a9afa96ad1aede174af9e5b1fbd4e
Kris <kmringler@gmail.com>
2020-06-11 21:54:04 +0000

    exercise complete

--- rotated-search/main.js
+++ rotated-search/main.js
@@ -4,5 +4,17 @@
  * @return {number}
  */
 var search = function (nums, target) {
+  //loop through array
+  //find target
+  //return target index
+  //if target doesnt exist, return -1
 
+  for (let i = 0; i < nums.length; i++) {
+    debugger;
+    if (nums[i] === target) {
+      return i;
+
+    }
+  }
+  return -1
 };

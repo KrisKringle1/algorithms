a0ffaa83b537a170b3ee0d7ddfd3f21ecfd26005
Kris <kmringler@gmail.com>
2020-06-11 23:10:26 +0000

    not quite sure i understand the question

--- max-subarray/main.js
+++ max-subarray/main.js
@@ -5,32 +5,12 @@
 var maxSubArray = function (nums) {
   //sort array
   //return the sum of the last 2 in the array
-  if (nums.length === 1) {
-    return nums[0]
+  let sum = nums[0]
+  let max = nums[0]
+  for (let i = 0; i < nums.length; i++) {
+    sum = Math.max(sum + nums[i], nums[i]),
+      max = Math.max(max, sum);
+    return max
+
   }
-
-  const sortedArray = nums.sort((a, b) => a - b);
-  const lastIndex = sortedArray.length - 1
-  let secondLastIndex = sortedArray.length - 2
-
-  if (sortedArray[lastIndex] === sortedArray[secondLastIndex]) {
-    while (sortedArray[secondLastIndex] === sortedArray[lastIndex]) {
-      secondLastIndex--;
-    }
-  }
-  const first = sortedArray[lastIndex];
-  const last = sortedArray[secondLastIndex];
-
-  if (Math.sign(first) === -1 && Math.sign(last) === -1) {
-    return last - first
-  }
-
-  const max = first + last;
-
-  if (max === -1 || max === 0) {
-    return 1
-  }
-
-
-  return max;
 };

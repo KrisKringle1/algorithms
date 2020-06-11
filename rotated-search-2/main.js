36985da475ce755afea6cd5a0f9a1e332edbd0e9
Kris <kmringler@gmail.com>
2020-06-11 22:13:56 +0000

    exercise complete

---
+++ rotated-search-2/main.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function (nums, target) {
+  //if array.includes target return true else return false
+
+  if (nums.includes(target)) {
+    return true
+  } else {
+    return false;
+  }
+};

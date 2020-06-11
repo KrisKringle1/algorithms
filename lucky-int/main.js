11a87b4e27daec1d40b40cb0fb462c976f0b747b
Kris <kmringler@gmail.com>
2020-06-11 20:06:05 +0000

    exercise complete

--- lucky-int/main.js
+++ lucky-int/main.js
@@ -4,24 +4,33 @@
  */
 var findLucky = function (arr) {
   //loop through array
-  //check if indexof is also last index of
-  //if it isnt, make it the lucky number
-  //if the next lucky number is larger, that is the returned number
-  let luckyNum = -1;
+  //put each element into an object
+  //count each element in object
+  //the object that has the same key value paring gets returned
+
+  const seen = {}
+  let luckyNum = null;
 
   for (let i = 0; i < arr.length; i++) {
-
-    if (arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i])) {
-      if (!luckyNum) {
-        luckyNum = arr[i]
-      }
-
-
-      if (luckyNum && arr[i] > luckyNum) {
-        luckyNum = arr[i];
-      }
+    if (!seen[arr[i]]) {
+      seen[arr[i]] = 1
+    } else {
+      seen[arr[i]]++
     }
-
   }
+
+  for (const key in seen) {
+    let numberKey = parseFloat(key)
+
+    if (numberKey === seen[key]) {
+      luckyNum = numberKey
+    }
+  }
+
+  if (luckyNum === null) {
+    luckyNum = -1
+  }
+
   return luckyNum
+
 };

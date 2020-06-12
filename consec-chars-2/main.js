0c34c75417e42e5cf6258bc4b60b9fd023851852
Kris <kmringler@gmail.com>
2020-06-12 19:43:31 +0000

    exercise complete

--- consec-chars-2/main.js
+++ consec-chars-2/main.js
@@ -1,10 +1,21 @@
 var findMaxConsecutiveOnes = function (s) {
   let i = 0
-  let counter = 1;
+  let counter = 0;
   let secondCounter = 0
+  debugger;
+  if (!s.includes(1)) {
+    return 0
+  }
+
+  if (s.indexOf(1) === s.lastIndexOf(1)) {
+    return 1
+  }
   while (i < s.length) {
 
-    if (s[i] === s[i + 1]) {
+    if (s[i] === s[i + 1] && s[i] === 1 && s[i + 1] === 1) {
+      if (!counter) {
+        counter = 1
+      }
       counter++;
     }
 
@@ -14,13 +25,16 @@ var findMaxConsecutiveOnes = function (s) {
 
       }
 
-      counter = 1;
+      counter = 0;
     }
 
     if (counter > secondCounter) {
       secondCounter = counter
     }
     i++
+  }
+  if (!secondCounter && s.includes(1)) {
+    secondCounter = 1
   }
   return secondCounter
 }

dd697d7e0077302d5c0fc7faacf3b716e805054d
Kris <kmringler@gmail.com>
2020-06-13 03:36:26 +0000

    finish tomorrow

--- majority-element-2/main.js
+++ majority-element-2/main.js
@@ -8,9 +8,13 @@ var majorityElement = function (nums) {
   //if array[i] isnt in object, create a key and give it value 1
   //for each value increase keyvalue
   //return an array of the values of the keys
-
+  if (nums.length === 1) {
+    return nums;
+  }
   const seen = {}
   const emptyArray = []
+  let highestValue = null
+  let highestKey;
 
   for (let i = 0; i < nums.length; i++) {
     if (!seen[nums[i]]) {
@@ -23,6 +27,18 @@ var majorityElement = function (nums) {
   }
 
   for (const key in seen) {
+    debugger;
+    if (!highestValue) {
+      highestValue = seen[key]
+      highestKey = parseFloat(key)
 
+    }
+
+    if (seen[key] > highestValue) {
+      highestValue = seen[key]
+      highestKey = parseFloat(key)
+      emptyArray.push(highestKey)
+    }
   }
+  return emptyArray
 };

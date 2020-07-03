1c6d828a14cafd1694dd335b52e6ebe9f2c6b8bc
Kris <kmringler@gmail.com>
2020-07-03 02:43:04 +0000

    exercise complete

--- maxProduct/main.js
+++ maxProduct/main.js
@@ -10,8 +10,8 @@ var maxProduct = function (nums) {
   //2nd number and first number becomes nums[i]
   //when loop finishes, subtract 1 from each number and multiply them together
 
-  let firstNumber;
-  let secondNumber;
+  let firstNumber = null;
+  let secondNumber = null;
 
   for (let i = 0; i < nums.length; i++) {
     debugger;
@@ -19,9 +19,14 @@ var maxProduct = function (nums) {
       firstNumber = nums[i]
     }
 
-    if (nums[i] > firstNumber) {
+
+    if (nums[i] >= firstNumber && i !== 0) {
       secondNumber = firstNumber
       firstNumber = nums[i]
+    }
+
+    if (nums[i] > secondNumber && nums[i] < firstNumber) {
+      secondNumber = nums[i]
     }
   }
   secondNumber--
@@ -29,3 +34,13 @@ var maxProduct = function (nums) {
   const product = firstNumber * secondNumber
   return product
 };
+
+
+function boo() {
+  const first = null
+
+  if (first) {
+    return true
+  }
+  return false
+}

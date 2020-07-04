93918ab1f69d2121ab7f543d14f8971697e43fca
Kris <kmringler@gmail.com>
2020-07-04 06:35:50 +0000

    exercise complete

--- jewels-and-stones/main.js
+++ jewels-and-stones/main.js
@@ -3,6 +3,19 @@
  * @param {string} S
  * @return {number}
  */
-var numJewelsInStones = function (J, S) {
+var numJewelsInStones = function (jewels, stones) {
+  let counter = 0
 
+
+  for (let i = 0; i < stones.length; i++) {
+    const letter = stones[i]
+    for (let j = 0; j < jewels.length; j++) {
+      if (jewels[j] === letter) {
+        counter++
+      }
+    }
+
+  }
+
+  return counter
 };

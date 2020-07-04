d08530162171c768e845e927f447d5a18765b50c
Kris <kmringler@gmail.com>
2020-07-04 21:42:41 +0000

    initial commit

---
+++ max-69-number/main.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number = function (num) {
+  //convert number to string
+  //loop through string   //while i is a 9 i ++
+  //when i is a 6 change to a 9
+  //convert back to number
+
+  //return value
+
+  const string = num + ""
+  let i = 0
+
+  while ()
+};

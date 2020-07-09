bfc0191b34a7d27c43850790723a19f2a8a0baf3
Kris <kmringler@gmail.com>
2020-07-09 02:34:53 +0000

    exercise complete

--- n-repeated-element/main.js
+++ n-repeated-element/main.js
@@ -2,6 +2,28 @@
  * @param {number[]} A
  * @return {number}
  */
-var repeatedNTimes = function (A) {
+var repeatedNTimes = function (array) {
+  //loop through array to find the element that is not equal to its last element
+  //assign that to a variabl
+  //foreach item in array, if its equal to variable, counter ++
+  //return counter
 
+  let i = 0
+
+  let repeatElement = null
+
+  while (repeatElement === null) {
+    debugger;
+    if (array.indexOf(array[i]) !== array.lastIndexOf(array[i])) {
+      repeatElement = array[i]
+    }
+
+    i++
+  }
+
+  if (repeatElement === null) {
+    return false
+  } else {
+    return repeatElement
+  }
 };

efd1a11b3ecba37051f956bd515c7700ab4fe051
Kris <kmringler@gmail.com>
2020-07-11 20:44:24 +0000

    inital logic added

--- baseball-game/main.js
+++ baseball-game/main.js
@@ -2,6 +2,54 @@
  * @param {string[]} ops
  * @return {number}
  */
-var calPoints = function (ops) {
+var calPoints = function (array) {
+  //create a counter variable thats starts at 0
+  //let mostRecent = most recent number added
+  //let secondRecent = 2nd most recent number
+  //loop through array
+  //switch statement
+  //case parseInt(element)
+  //add to counter
+  //case C, remove [i-1] from counter
+  //case D, double Numbers
 
+  let score = 0
+  let mostRecent = null
+  let secondMostRecent = null
+  let thirdMostRecent = null
+
+  for (let i = 0; i < array.length; i++) {
+
+    if (parseInt(array[i])) {
+      score += parseInt(array[i])
+      secondMostRecent = mostRecent
+      thirdMostRecent = secondMostRecent
+      mostRecent = array[i]
+
+
+    } else {
+      switch (array[i]) {
+
+
+        case "D":
+
+          secondMostRecent = mostRecent
+          mostRecent = score * 2
+          score = score + (score * 2)
+
+          break;
+        case "C":
+          score = score - parseInt(mostRecent)
+          mostRecent = secondMostRecent;
+          secondMostRecent = thirdMostRecent
+          break;
+        case "+":
+          score = score + parseInt(secondMostRecent) + parseInt(mostRecent)
+          break;
+
+      }
+    }
+  }
+
+  return score
 };

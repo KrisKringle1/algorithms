2dc314bbb9cca1d44e4f754f62ba71dd205c15d8
Kris <kmringler@gmail.com>
2020-07-11 19:24:20 +0000

    initial commit

---
+++ reformate-date/main.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} date
+ * @return {string}
+ */
+var reformatDate = function (date) {
+  //check the length of the string to run 2 different code blocks
+
+  //loop through string
+  //push the first 2 elements into a new variable set as day
+  //push the middle 3 indexes together to make a variable month
+  //push the last 4 elements of the string into a new variable "year"
+  //create a return string that matches the desired output
+
+
+
+};

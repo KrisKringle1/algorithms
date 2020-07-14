9b2c97db923703e6b078455fc76868f58321f9a0
Kris <kmringler@gmail.com>
2020-07-14 04:58:55 +0000

    intiial commit

---
+++ stone-game/main.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function (stones) {
+  //loop through array and assign the 2 largest elements to variables
+  //assign their indexes to their variables as well
+  //splice out the 2 numbers out of the array
+  //if > 0, push to the array
+  //while array.length > 1 continue
+
+  let firstNumber = stones[0]
+  let secondNumber = stones[1]
+};

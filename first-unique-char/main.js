256a39a109345243576c242192e7c24aa9a69cee
Kris <kmringler@gmail.com>
2020-07-15 01:00:42 +0000

    finish

--- first-unique-char/main.js
+++ first-unique-char/main.js
@@ -3,8 +3,12 @@
  * @return {number}
  */
 var firstUniqChar = function (s) {
-
+  if (!s || s.length === 2) {
+    return -1
+  }
   for (let i = 0; i < s.length; i++) {
-    if (s.indexOf(s[i])
+    if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+      return i
+    }
   }
 };

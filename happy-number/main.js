b682db0ea2ee15c68007dcda2f9ed674c52a2462
Kris <kmringler@gmail.com>
2020-07-17 02:42:17 +0000

    initial commit

---
+++ happy-number/main.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function (n) {
+  //convert number to string
+  //loop through the string
+  //for each element in the string, square it and add  it together and repeat
+  //if outcome is 1 return true
+  //if outcome is 0 return false
+
+};

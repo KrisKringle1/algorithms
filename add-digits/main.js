64e3d607c3a61ebfedfb26e65424aae9a39cf783
Kris <kmringler@gmail.com>
2020-07-18 22:19:31 +0000

    what to do with my accumulator

--- add-digits/main.js
+++ add-digits/main.js
@@ -6,32 +6,11 @@ var addDigits = function (num) {
   //turn number into a string
   //while string.length > 1, add the 2 digets together
   //return the number
-  let string = undefined
-  debugger;
+  const string = num.toString()
+  let number = 0
 
-  let number = 5;
-
-  function accumulator(string) {
-    debugger;
-
-
-    for (let i = 0; i < string.length; i++) {
-      if (i === 0) {
-        number = 0
-      }
-
-      number = number + parseInt(string[i])
-    }
-    string = number.toString()
+  for (let i = 0; i < string.length; i++) {
+    number = number + string[i]
   }
-
-  while (number.toString() > 1) {
-    if (string === undefined) {
-      string = num.toString()
-    }
-    accumulator(string)
-  }
-
-  return number
 
 };

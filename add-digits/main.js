ff8cdc0cb8d077be7d2b90f4b7df29081c786bdf
Kris <kmringler@gmail.com>
2020-07-18 21:59:48 +0000

    more logic

--- add-digits/main.js
+++ add-digits/main.js
@@ -6,20 +6,29 @@ var addDigits = function (num) {
   //turn number into a string
   //while string.length > 1, add the 2 digets together
   //return the number
-  let string = num.toString()
+  let string = undefined
   debugger;
 
-  let number = 0
+  let number = 5;
 
   function accumulator(string) {
     debugger;
+
+
     for (let i = 0; i < string.length; i++) {
+      if (i === 0) {
+        number = 0
+      }
+
       number = number + parseInt(string[i])
     }
+    string = number.toString()
   }
 
   while (number.toString() > 1) {
-    debugger;
+    if (string === undefined) {
+      string = num.toString()
+    }
     accumulator(string)
   }
 

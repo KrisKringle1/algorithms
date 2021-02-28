9ff759847f6e217200e1e434fa76eb939390f3b9
KrisKringle1 <kmringler@gmail.com>
2021-02-27 23:52:54 -0800

    test

--- single-number-3/main.js
+++ single-number-3/main.js
@@ -6,13 +6,13 @@ var singleNumber = function (nums) {
   //loop through each element in array
   //if first index === lastindex, push to new array
   //return new array
+  var hello = "hello"
   const newArray = [];
 
   for (let i = 0; i < nums.length; i++) {
     if (nums.indexOf(nums[i]) === nums.lastIndexOf(nums[i])) {
       newArray.push(nums[i])
     }
-
 
 
   }

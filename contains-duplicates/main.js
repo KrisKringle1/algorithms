b614927f351ce5de619529cc1b3b6ed29a9bf43b
Kris Ringler <kmringler@Kriss-Air-2.lan>
2022-03-13 17:29:20 -0700

    completed

--- contains-duplicates/main.js
+++ contains-duplicates/main.js
@@ -7,4 +7,17 @@ var containsDuplicate = function (nums) {
   //loop through each number in numbers and store it in the object literal
   //if number exists already in object, return true
   //if loop finishes return false
+
+  const storage = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const currNumber = nums[i] + "";
+
+    if (!storage[currNumber]) {
+      storage[currNumber] = currNumber;
+    } else {
+      return true;
+    }
+  }
+  return false;
 };

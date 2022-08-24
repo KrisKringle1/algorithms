5b58b42d956d3c6ed104a75c30b4ed26962c0c30
Kris Ringler <kmringler@Kriss-Air-2.lan>
2022-08-24 15:22:37 -0700

    sunsequence

---
+++ subsequence/main.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+  //loop through each character in string and check if its included in t
+  //if it isnt, return false
+  //if it is, get the index of that character
+  //continue the loop
+  //if the next character is in the string and the index is greater than the 1st index, continue
+  //reset the index variable to be the newest character,
+  //repeat
+};

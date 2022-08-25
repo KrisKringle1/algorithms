598c5178c051df667eb151f216e6236565c5dd19
Kris Ringler <kmringler@gmail.com>
2022-08-25 15:45:39 -0700

    ransom note completed

--- ransom-note/main.js
+++ ransom-note/main.js
@@ -8,4 +8,14 @@ var canConstruct = function (ransomNote, magazine) {
   //if character is in magazine, splice it out
   //if its not, return false
   //if loop completes return true
+
+  for (let i = 0; i < ransomNote.length; i++) {
+    const char = ransomNote[i];
+    if (magazine.includes(char)) {
+      magazine = magazine.replace(char, "_");
+    } else {
+      return false;
+    }
+  }
+  return true;
 };

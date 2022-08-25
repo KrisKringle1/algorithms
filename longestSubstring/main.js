3aee076654c64ca6da6f65965bb59b3e64f5b82d
Kris Ringler <kmringler@Kriss-Air-2.lan>
2022-08-24 19:13:32 -0700

    local substring added

---
+++ longestSubstring/main.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  /* 
+    define 2 string variable thats, first string second string
+    begin looping through s
+    if curr char is not in our first string variable
+    
+    if cur char is in the string and the loop is still going
+    go through the same logic but start adding chars to the 2nd string
+
+    repeat logic, and delete string that is shorter
+    */
+
+  let firstString = "";
+  let secondString = "";
+
+  for (let i = 0; i < s.length; i++) {}
+};
--- subsequence/main.js
+++ subsequence/main.js
@@ -11,4 +11,36 @@ var isSubsequence = function (s, t) {
   //if the next character is in the string and the index is greater than the 1st index, continue
   //reset the index variable to be the newest character,
   //repeat
+  let index;
+  let string = t;
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+
+    if (string.includes(char)) {
+      if (!index) {
+        index = t.indexOf(char);
+        string = string.replace(char, "_");
+        console.log("char ", char);
+        console.log("index ", index);
+        console.log("string ", string);
+        continue;
+      }
+
+      if (index > t.indexOf(char)) {
+        console.log("char ", char);
+        console.log("index ", index);
+        console.log("index of ", t.indexOf(char));
+        console.log("string ", string);
+        return false;
+      } else {
+        index = t.indexOf(char);
+        string = string.replace(char, "_");
+        console.log(string);
+      }
+    } else {
+      return false;
+    }
+  }
+
+  return true;
 };

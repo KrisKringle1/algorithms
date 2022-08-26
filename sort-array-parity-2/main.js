e7b2625aea9f248f3b51bf8882a861778a47284e
Kris Ringler <kmringler@gmail.com>
2022-08-26 10:52:17 -0700

    slowwwwwwww solution

--- sort-array-parity-2/main.js
+++ sort-array-parity-2/main.js
@@ -2,4 +2,26 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortArrayByParityII = function (nums) {};
+var sortArrayByParityII = function (nums) {
+  //create a return array
+  //loop through numbers
+  // if i % 2 === 0, find an event in nums and put it in that index
+  //else find an odd number and put it in that index
+  const sorted = [];
+  const length = nums.length;
+  for (let i = 0; i < length; i++) {
+    if (i % 2 === 0) {
+      const even = nums.find((element) => element % 2 === 0);
+      const index = nums.indexOf(even);
+      sorted.push(even);
+      nums.splice(index, 1);
+    } else {
+      const odd = nums.find((element) => element % 2 > 0);
+      const index = nums.indexOf(odd);
+      sorted.push(odd);
+      nums.splice(index, 1);
+    }
+  }
+
+  return sorted;
+};

b06235c85ba2d23fbe89ea9398914f15331f9651
Kris Ringler <kmringler@gmail.com>
2022-08-26 11:03:33 -0700

    largest int

--- sort-array-parity-2/main.js
+++ sort-array-parity-2/main.js
@@ -7,21 +7,49 @@ var sortArrayByParityII = function (nums) {
   //loop through numbers
   // if i % 2 === 0, find an event in nums and put it in that index
   //else find an odd number and put it in that index
-  const sorted = [];
-  const length = nums.length;
-  for (let i = 0; i < length; i++) {
+  //   const sorted = [];
+  //   const length = nums.length;
+  //   for (let i = 0; i < length; i++) {
+  //     if (i % 2 === 0) {
+  //       const even = nums.find((element) => element % 2 === 0);
+  //       const index = nums.indexOf(even);
+  //       sorted.push(even);
+  //       nums.splice(index, 1);
+  //     } else {
+  //       const odd = nums.find((element) => element % 2 > 0);
+  //       const index = nums.indexOf(odd);
+  //       sorted.push(odd);
+  //       nums.splice(index, 1);
+  //     }
+  //   }
+
+  //   return sorted;
+
+  for (let i = 0; i < nums.length; i++) {
+    const curr = nums[i];
+
     if (i % 2 === 0) {
-      const even = nums.find((element) => element % 2 === 0);
-      const index = nums.indexOf(even);
-      sorted.push(even);
-      nums.splice(index, 1);
-    } else {
-      const odd = nums.find((element) => element % 2 > 0);
-      const index = nums.indexOf(odd);
-      sorted.push(odd);
-      nums.splice(index, 1);
+      if (curr % 2 === 0) {
+        continue;
+      } else {
+        const index = nums.findIndex((element) => element % 2 === 0);
+        const switchNum = nums[index];
+        nums[i] = switchNum;
+        nums[index] = curr;
+      }
+    }
+
+    if (i % 2 > 0) {
+      if (curr % 2 > 0) {
+        continue;
+      } else {
+        const index = nums.findIndex((element) => element % 2 === 0);
+        const switchNum = nums[index];
+        nums[i] = switchNum;
+        nums[index] = curr;
+      }
     }
   }
 
-  return sorted;
+  return nums;
 };

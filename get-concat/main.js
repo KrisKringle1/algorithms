1d43a7de0199aef44df82d2ca641c61c1b1ecb7d
Kris Ringler <kmringler@gmail.com>
2022-08-26 14:39:16 -0700

    concat success

--- get-concat/main.js
+++ get-concat/main.js
@@ -2,4 +2,13 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var getConcatenation = function (nums) {};
+var getConcatenation = function (nums) {
+  const length = nums.length;
+
+  for (let i = 0; i < length; i++) {
+    const curr = nums[i];
+    nums.push(curr);
+  }
+
+  return nums;
+};

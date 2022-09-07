d469349bf5a7eb53cb498907e8b20b23b9152835
Kris Ringler <kmringler@gmail.com>
2022-09-07 16:59:05 -0700

    valid paren complete

--- vaid-parenthesis/main.js
+++ vaid-parenthesis/main.js
@@ -2,4 +2,22 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {};
+var isValid = function (s) {
+  const stack = [];
+  const legend = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    const curr = s[i];
+    if (curr === "(" || curr === "{" || curr === "[") {
+      stack.push(curr);
+    } else if (legend[stack.pop()] !== curr) {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+};

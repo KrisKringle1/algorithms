a661d0b0376411b653960f8ffeba32d8989eed27
Kris Ringler <kmringler@gmail.com>
2022-09-06 21:07:20 -0700

    delete duplicates

---
+++ delete-duplicates/main.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  //loop through head
+
+  //if firstIndex is not the last index of
+  //or if number is in seen object
+  //splice out number
+  //continue looping
+
+  const seen = {};
+};

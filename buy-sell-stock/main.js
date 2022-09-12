2480b8443eff85e6da7ec2d4fdefbd8059fdd9f9
Kris Ringler <kmringler@gmail.com>
2022-09-12 14:24:39 -0700

    stocks complete

--- buy-sell-stock/main.js
+++ buy-sell-stock/main.js
@@ -2,4 +2,18 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (prices) {};
+var maxProfit = function (prices) {
+  let max = 0;
+  let min = null;
+
+  for (let i = 0; i < prices.length; i++) {
+    const curr = prices[i];
+
+    if (curr < min || min === null) {
+      min = curr;
+    } else {
+      max = Math.max(max, curr - min);
+    }
+  }
+  return max;
+};

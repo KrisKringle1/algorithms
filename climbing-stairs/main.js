b14f648d28de1eef73b61a877e05241e72d3b87f
Kris Ringler <kmringler@gmail.com>
2022-09-13 16:38:01 -0700

    climbing stairs

---
+++ climbing-stairs/main.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  const countingFunction = (stairsRemaining, savedResults) => {};
+
+  return countingFunction(n, {});
+};

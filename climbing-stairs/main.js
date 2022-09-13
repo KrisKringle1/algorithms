2f9bbec8250c984d2df7b367f52697b4f5798195
Kris Ringler <kmringler@gmail.com>
2022-09-13 16:45:30 -0700

    stair climber complete

--- climbing-stairs/main.js
+++ climbing-stairs/main.js
@@ -3,7 +3,23 @@
  * @return {number}
  */
 var climbStairs = function (n) {
-  const countingFunction = (stairsRemaining, savedResults) => {};
+  const countingFunction = (stairsRemaining, savedResults) => {
+    if (stairsRemaining < 0) {
+      return 0;
+    }
+    if (stairsRemaining === 0) {
+      return 1;
+    }
+
+    if (savedResults[stairsRemaining]) {
+      return savedResults[stairsRemaining];
+    }
+
+    savedResults[stairsRemaining] =
+      countingFunction(stairsRemaining - 1, savedResults) +
+      countingFunction(stairsRemaining - 2, savedResults);
+    return savedResults[stairsRemaining];
+  };
 
   return countingFunction(n, {});
 };

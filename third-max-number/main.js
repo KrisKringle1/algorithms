02d48cf6d854ad90c081751798f0ee03b440e928
Kris Ringler <kmringler@gmail.com>
2022-09-22 20:09:12 -0700

    second try

--- third-max-number/main.js
+++ third-max-number/main.js
@@ -9,13 +9,21 @@ var thirdMax = function (nums) {
   //if nums i > first number, second number
   //if nums i > first number and nums i greater than 2nd number, define 2nd number
 
-  let firstMax = 0;
-  let secondMax = 0;
-  let thirdMax = 0;
+  let firstMax = null;
+  let secondMax = null;
+  let thirdMax = null;
   if (nums.length === 2) return Math.max(nums[0], nums[1]);
 
   for (let i = 0; i < nums.length; i++) {
     const curr = nums[i];
+    if (thirdMax === null) {
+      thirdMax = curr;
+    } else if (secondMax === null) {
+      secondMax === curr;
+    } else if (firstMax === null) {
+      firstMax = curr;
+    }
+
     if (curr > thirdMax) {
       firstMax = secondMax;
       secondMax = thirdMax;

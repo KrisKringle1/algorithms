a08825f56c31d2ce40e103f25a8268fc710224d0
Kris Ringler <kmringler@gmail.com>
2022-09-22 19:38:46 -0700

    first attempt

--- island-perimiter/main.js
+++ island-perimiter/main.js
@@ -2,4 +2,10 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var islandPerimeter = function (grid) {};
+var islandPerimeter = function (grid) {
+  //check each item in the grid
+  //if i === 1, perimiter + 4
+  //if i > O && grid above is 1, perimiter - 2
+  //if j > 0 and grid before is a 1, perimiter - 2
+  //return perimiter
+};
--- summary-ranges/main.js
+++ summary-ranges/main.js
@@ -5,4 +5,31 @@
 var summaryRanges = function (nums) {
   //loop through each character in nums
   //let firstNum = nums[i]
+  //loop through each number
+  //while nums[i] + 1 ===  nums[j]
+  //continue
+  //whne they dont, push them into a range
+  //return the array of ranges
+  let firstNum = null;
+  let secondNum = null;
+  const ranges = [];
+  for (let i = 0; i < nums.length; i++) {
+    let j = i + 1;
+    if (!firstNum) {
+      firstNum = nums[i];
+    }
+
+    if (nums[i] + 1 !== nums[j]) {
+      secondNum = nums[i];
+      const rangeString = `${firstNum}->${secondNum}`;
+      if (firstNum === secondNum) {
+        ranges.push(firstNum.toString());
+      } else {
+        ranges.push(rangeString);
+      }
+      firstNum = null;
+      secondNum = null;
+    }
+  }
+  return ranges;
 };
